bb3a24e17d0a8cbcb03857690bc2521270f09dba
mrmedrano81 <michael.medrano@eee.upd.edu.ph>
2023-07-01 22:26:24 +0800

    tested some mlperf training

--- model_training/MLPerf/training/keyword_spotting/build_ds_cnn.sh
+++ model_training/MLPerf/training/keyword_spotting/build_ds_cnn.sh
@@ -1,21 +1,21 @@
 # suppress informational messages from TF
 export TF_CPP_MIN_LOG_LEVEL=2 
 
-python train.py --saved_model_path=trained_models/ds_cnn_3.h5 \
+python train.py --saved_model_path=trained_models/ds_cnn_199_v2.h5 \
        --epochs=30  \
        --run_test_set=True \
        --data_dir data \
        --model_architecture=ds_cnn \
-       --mlflow_run_name=ds_cnn_3  \
+       --mlflow_run_name=ds_cnn_199_v2  \
        --feature_type=mfcc \
        --dct_coefficient_count 40
 
-python quantize.py --saved_model_path=trained_models/ds_cnn_3.h5 \
-       --tfl_file_name=trained_models/ds_cnn_3.tflite \
+python quantize.py --saved_model_path=trained_models/ds_cnn_199_v2.h5 \
+       --tfl_file_name=trained_models/ds_cnn_199_v2.tflite \
        --data_dir data \
        --dct_coefficient_count 40
 
-python eval_quantized_model.py --tfl_file_name=trained_models/ds_cnn_3.tflite \
+python eval_quantized_model.py --tfl_file_name=trained_models/ds_cnn_199_v2.tflite \
        --data_dir data \
        --target_set=test \
        --feature_type=mfcc \
